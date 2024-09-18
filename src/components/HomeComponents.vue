54f94e41f9d8cad749df8cffd6bad26180dc73d1
Victor Notaro <victorhugonotaro@icloud.com>
2024-09-18 00:05:39 -0300

    first commit

---
+++ src/components/HomeComponents.vue
@@ -0,0 +1,161 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+import Button from './ButtonComponents.vue';
+
+const products = ref([]);
+
+const fetchProducts = async () => {
+  try {
+    const response = await axios.get('https://localhost:7077/produtos');
+    products.value = response.data;
+  } catch (error) {
+    console.error('Erro ao buscar os produtos:', error);
+  }
+};
+
+onMounted(() => {
+  fetchProducts();
+});
+</script>
+
+<template>
+  <div>
+    <Navbar />
+  </div>
+
+  <div class="container">
+    <div class="row">
+      <img src="/src/assets/images/VIMA.png" alt="Imagem de login" class="login-image">
+    </div>
+  </div>
+
+  <div class="divisor"></div>
+
+  <div class="text-center">
+    <p class="sub-title" style="margin-bottom: 1vh;"><b>PRODUTOS EM DESTAQUE</b></p>
+  </div>
+
+  <div class="container">
+    <div class="row">
+      <div v-for="product in products" :key="product.id" class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+        <div class="card">
+          <div class="image">
+            <img :src="product.imageURL" alt="Imagem do produto" />
+          </div>
+          <div class="card-body">
+            <span class="title">{{ product.nome }}</span>
+            <span class="price"><b>R$ {{ product.preco }}</b></span>
+           
+            <router-link class="nav-link" aria-current="page" to="/product">
+              <button class="custom-button mt-3">Ver Agora &#8594;</button>
+            </router-link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  position: relative;
+  width: 100%;
+  height: 35.5em;
+  box-shadow: 0px 1px 13px rgba(0, 0, 0, 0.1);
+  cursor: pointer;
+  transition: all 120ms;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #ffffff;
+  padding: 0.5em;
+  padding-bottom: 3.4em;
+  overflow: hidden;
+}
+
+.custom-button {
+  background-color: #00AC7C;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  text-transform: uppercase;
+  font-weight: 600;
+  width: 100%;
+  position: absolute;
+  left: 0;
+  bottom: -60px;
+  transition: all 80ms;
+  height: 5em;
+  opacity: 0;
+}
+
+.card:hover .custom-button {
+  bottom: 0;
+  opacity: 1;
+}
+
+.card:active .custom-button {
+  background-color: #007b63;
+  transform: scale(0.98);
+}
+
+.card .title {
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 1.4em;
+  position: absolute;
+  left: 0.625em;
+  bottom: 1.875em;
+  font-weight: 400;
+  color: #000;
+}
+
+.card .price {
+  font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+  font-size: 1.1em;
+  position: absolute;
+  left: 0.625em;
+  bottom: 0.625em;
+  color: #000;
+}
+
+.image {
+  background: rgb(241, 241, 241);
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.image img {
+  max-width: 100%;
+  max-height: 100%;
+  object-fit: contain;
+}
+
+.container {
+  background-color: rgba(0, 0, 0, 0);
+  margin-top: 5vh;
+}
+
+.container img {
+  height: 36vh;
+}
+
+.title-text {
+  font-size: 10vh;
+}
+
+.sub-title {
+  font-size: 4vh;
+  margin-top: 3vh;
+}
+
+.divisor {
+  background-color: #212529;
+  height: 5vh;
+  margin-top: 6cap;
+  margin-bottom: 5vh;
+}
+</style>
