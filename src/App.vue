18aa99e959c25d25c7c83b174430a0b6f35530a1
Victor Notaro <victorhugonotaro@icloud.com>
2024-09-25 20:48:40 -0300

    Criação do footer

--- src/App.vue
+++ src/App.vue
@@ -55,7 +55,7 @@
       <p>&copy; 2024 Sua Empresa. Todos os direitos reservados.</p>
       <p>
         <a href="#" class="text-white me-2">Política de Privacidade</a>
-        <a href="#" class="text-white">Termos de Uso</a>
+        <a href="#" class="text-white">FAQ</a>
       </p>
     </div>
   </footer>
