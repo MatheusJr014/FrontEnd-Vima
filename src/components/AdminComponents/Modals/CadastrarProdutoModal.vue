61c2328e369b49e7cb935194bf32b7a365291bba
Matheus <bonecodoido339@gmail.com>
2024-11-13 00:41:14 -0300

    Foi melhorado o estilo da pagina para melhor apresentação

--- src/components/AdminComponents/Modals/CadastrarProdutoModal.vue
+++ src/components/AdminComponents/Modals/CadastrarProdutoModal.vue
@@ -1,18 +1,24 @@
 <template>
   <div>
-    <!-- Button trigger modal -->
+    <!-- Botão para abrir o modal de cadastro -->
     <button type="button" class="btn btn-primary" @click="showModal = true">
       Cadastrar Produto
     </button>
 
-    <!-- Modal -->
-    <div class="modal fade" :class="{ show: showModal }" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" v-show="showModal">
+    <!-- Modal de Cadastro de Produto -->
+    <div class="modal fade" :class="{ show: showModal }" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+      aria-hidden="true" v-show="showModal">
       <div class="modal-dialog" role="document">
         <div class="modal-content">
           <div class="modal-header">
             <h5 class="modal-title" id="exampleModalLabel">Cadastrar Produto</h5>
-            <button type="button" class="close" @click="closeModal">
-              <span aria-hidden="true">&times;</span>
+            <button type="button" class="close close-button" @click="closeModal">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle"
+                viewBox="0 0 16 16">
+                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                <path
+                  d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
+              </svg>
             </button>
           </div>
           <div class="modal-body">
@@ -51,16 +57,40 @@
         </div>
       </div>
     </div>
+
+    <!-- Modal de Sucesso Personalizado -->
+    <div class="notifications-container" v-show="showSuccessMessage">
+      <div class="success">
+        <div class="flex">
+          <div class="flex-shrink-0">
+            <svg aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" class="succes-svg">
+              <path clip-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" fill-rule="evenodd"></path>
+            </svg>
+          </div>
+          <div class="success-prompt-wrap">
+            <p class="success-prompt-heading">Produto cadastrado com sucesso!</p>
+            <div class="success-prompt-prompt">
+              <p>O produto foi adicionado ao inventário com sucesso.</p>
+            </div>
+            <div class="success-button-container">
+              <button class="success-button-main" type="button" @click="showSuccessMessage = false">Fechar</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import ProdutoDataService from '@/services/AdminService/ProdutoDataService.js';
 import '@/assets/styles/Admin/CadastrarProdutoModal.css';
+
 export default {
   data() {
     return {
       showModal: false,
+      showSuccessMessage: false,
       userData: {
         nome: "",
         preco: 0,
@@ -75,23 +105,28 @@ export default {
   },
   methods: {
     saveProduct() {
-      // Transformar o input de tamanhos em um array
       this.userData.tamanhos = this.userData.tamanhosInput.split(',').map(tamanho => tamanho.trim());
 
-      // Enviar os dados para o serviço
       ProdutoDataService.create(this.userData)
         .then(response => {
-          console.log(response.data); 
-          this.closeModal(); // Fechar modal após salvar com sucesso
+          console.log(response.data);
+          this.showSuccessMessage = true; // Exibe o modal de sucesso
+          this.closeModal();
+
+          // Esconde o modal de sucesso após 3 segundos
+          setTimeout(() => {
+            this.showSuccessMessage = false;
+          }, 3000);
         })
         .catch(error => {
           console.error("Erro ao salvar o produto:", error);
         });
     },
     closeModal() {
-      this.showModal = false; // Fechar o modal
+      this.showModal = false;
     }
   }
 };
 </script>
+
 
